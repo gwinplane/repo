6b5dffa9363f7f86db4d929c7f101b549806f425
gwinplane <maxmarinin@gmail.com>
2026-01-15 14:17:19 +0100

    visual studio repo

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessons
+{
+    class Program
+    {
+       static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo, Welt!");
+            Console.WriteLine("Ich heiße Maksym");
+            Console.WriteLine("Ich lerne C#");
+            Console.ReadLine();
+        }
+    }
+}
