3ed6a5b4a3642640b8030315c38194088968d1da
gwinplane <maxmarinin@gmail.com>
2026-01-17 18:37:26 +0100

    first day

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,9 +11,35 @@ namespace Lessons
     {
        static void Main(string[] args)
         {
-            Console.WriteLine("Hallo, Welt!");
-            Console.WriteLine("Ich heiße Maksym");
-            Console.WriteLine("Ich lerne C#");
+            Console.WriteLine("Name des Helden");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hallo, {name}!");
+
+            Console.WriteLine("Magischer Gegenstand");
+            string gegenstand = Console.ReadLine();
+            Console.WriteLine($"Dein Gegenstand ist {gegenstand}!");
+
+            Console.WriteLine("Was ist dein Ort?");
+            string ort = Console.ReadLine();
+            Console.WriteLine($"Dein Ort ist {ort}!");
+
+      
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("=== DEIN FRAGEBOGEN ===");
+            Console.WriteLine($"Name des Helden: {name}");
+            Console.WriteLine();
+            Console.WriteLine($"Magischer Gegenstand: {gegenstand}");
+            Console.WriteLine();
+            Console.WriteLine($"Ort: {ort}");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine($"Eines Tages fand {name} einen {gegenstand}.");
+            Console.WriteLine($"Mit diesem Gegenstand ging er zum {ort}.");
+            Console.WriteLine($"Dort begann ein unglaubliches Abenteuer!");
+
+
             Console.ReadLine();
         }
     }
