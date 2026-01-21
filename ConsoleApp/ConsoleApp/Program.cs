f083f8248aae2adc12f7f76f0cfd256fa9ddba65
gwinplane <maxmarinin@gmail.com>
2026-01-21 17:06:11 +0100

    gersde ungerade

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -7,18 +7,18 @@ namespace Lessons
     {
        static void Main(string[] args)
         {
-            Console.WriteLine("Note1:");
-            double note1 = double.Parse(Console.ReadLine());
-            Console.WriteLine("Note2:");
-            double note2 = double.Parse(Console.ReadLine());
-            Console.WriteLine("Note3:");
-            double note3 = double.Parse(Console.ReadLine());
-
-            //double durchschnitt = (note1 + note2 + note3) / 3;
-            //Console.WriteLine($"Durchschnitt: {durchschnitt}");
-
-            //Console.WriteLine($"Durchschnitt: {(note1+note2+note3)/3}");
-            Console.WriteLine($"Durchschnitt: {Math.Round((note1 + note2 + note3) / 3, 2)}");
+            Console.WriteLine("Geben Sie bitte eine Zahl ein:");
+            int zahl = int.Parse(Console.ReadLine());
+            int rest = zahl % 2;
+            Console.WriteLine($"{zahl} % 2 = {rest}");
+            if (rest == 0)
+            {
+                Console.WriteLine("(Die Zahl ist gerade)");
+            }
+            else
+            {
+                Console.WriteLine("Die Zahl ist ungerade");
+            }
 
 
 
--- ConsoleApp/ConsoleApp/obj/Debug/net10.0/ConsoleApp.AssemblyInfo.cs
+++ ConsoleApp/ConsoleApp/obj/Debug/net10.0/ConsoleApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f23bb1b1983c5b4879eb0e8e7b7af50463c40216")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c84024d055484c784bff0da2acba72d9cecee7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
