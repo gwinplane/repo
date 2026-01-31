ab1e5a61fa24b6b3f59fa6625fbac0f91c503995
gwinplane <maxmarinin@gmail.com>
2026-01-31 18:03:42 +0100

    Renaming

---
+++ Address Management System (Console App)/SQL/database.sql
@@ -0,0 +1,11 @@
+﻿CREATE DATABASE address_db;
+USE address_db;
+
+CREATE TABLE addresses (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    street VARCHAR(100),
+    house_number VARCHAR(20),
+    postal_code VARCHAR(20),
+    city VARCHAR(100),
+    country VARCHAR(100)
+);
